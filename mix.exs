07b11d8c2df802060b11a7ec5a5625f0da43eae4
brandynbennett <brandynlbennett@gmail.com>
2022-05-26 09:35:26 -0600

    add some deps

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule PolynMigrator.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -22,8 +23,24 @@ defmodule PolynMigrator.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_json_schema, "~> 0.9.1"},
+      {:jason, "~> 1.2"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:jetstream, git: "https://github.com/mmmries/jetstream"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to create, migrate and run the seeds file at once:
+  #
+  #     $ mix ecto.setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    [
+      lint: ["credo --strict"]
     ]
   end
 end
