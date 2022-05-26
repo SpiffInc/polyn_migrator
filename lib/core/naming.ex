2df5fd4d692df1a819d70ec53e0c6e83befd26dc
brandynbennett <brandynlbennett@gmail.com>
2022-05-26 10:18:49 -0600

    add naming

---
+++ lib/core/naming.ex
@@ -0,0 +1,85 @@
+defmodule Polyn.Naming do
+  @moduledoc """
+  Utilities for working with Polyn naming conventions
+  """
+
+  @doc """
+  Convert a dot separated name into a colon separated name
+
+  ## Examples
+
+      iex>Polyn.Naming.dot_to_colon("com.acme.user.created.v1.schema.v1")
+      "com:acme:user:created:v1:schema:v1"
+  """
+  @spec dot_to_colon(str :: binary()) :: binary()
+  def dot_to_colon(str) do
+    String.replace(str, ".", ":")
+  end
+
+  @doc """
+  Convert a colon separated name into a dot separated name
+
+  ## Examples
+
+      iex>Polyn.Naming.colon_to_dot("com:acme:user:created:v1:schema:v1")
+      "com.acme.user.created.v1.schema.v1"
+  """
+  @spec colon_to_dot(str :: binary()) :: binary()
+  def colon_to_dot(str) do
+    String.replace(str, ":", ".")
+  end
+
+  @doc """
+  Remove the `:domain` prefix from a name
+
+  ## Examples
+
+      iex>Polyn.Naming.trim_domain_prefix("com:acme:user:created:v1:schema:v1")
+      "user:created:v1:schema:v1"
+
+      iex>Polyn.Naming.trim_domain_prefix("com.acme.user.created.v1.schema.v1")
+      "user.created.v1.schema.v1"
+  """
+  @spec trim_domain_prefix(str :: binary()) :: binary()
+  def trim_domain_prefix(str) do
+    String.replace(str, "#{domain()}.", "", global: false)
+    |> String.replace("#{dot_to_colon(domain())}:", "", global: false)
+  end
+
+  @doc """
+  Give a version number to a name
+
+  ## Examples
+
+      iex>Polyn.Naming.version_suffix("com:acme:user:created:")
+      "com:acme:user:created:v1"
+
+      iex>Polyn.Naming.version_suffix("com.acme.user.created.", 2)
+      "com.acme.user.created.v2"
+  """
+  @spec version_suffix(str :: binary(), version :: non_neg_integer()) :: binary()
+  @spec version_suffix(str :: binary()) :: binary()
+  def version_suffix(str, version \\ 1) do
+    "#{str}v#{version}"
+  end
+
+  @doc """
+  Remove the version suffix from a name
+
+  ## Examples
+
+      iex>Polyn.Naming.trim_version_suffix("com.acme.user.created.v1")
+      "com.acme.user.created"
+
+      iex>Polyn.Naming.trim_version_suffix("com:acme:user:created:v1")
+      "com:acme:user:created"
+  """
+  @spec trim_version_suffix(str :: binary()) :: binary()
+  def trim_version_suffix(str) do
+    String.replace(str, ~r/[\.\:]+v\d+/, "")
+  end
+
+  defp domain do
+    Application.fetch_env!(:polyn_migrator, :domain)
+  end
+end
---
+++ test/core/naming_test.exs
@@ -0,0 +1,52 @@
+defmodule Polyn.NamingTest do
+  use ExUnit.Case, async: true
+
+  alias Polyn.Naming
+
+  test "dot_to_colon/1" do
+    assert "com:acme:user:created:v1:schema:v1" ==
+             Naming.dot_to_colon("com.acme.user.created.v1.schema.v1")
+  end
+
+  test "colon_to_dot/1" do
+    assert "com.acme.user.created.v1.schema.v1" ==
+             Naming.colon_to_dot("com:acme:user:created:v1:schema:v1")
+  end
+
+  describe "trim_domain_prefix/1" do
+    test "removes prefix when dots" do
+      assert "user.created.v1.schema.v1" ==
+               Naming.trim_domain_prefix("com.test.user.created.v1.schema.v1")
+    end
+
+    test "removes prefix when colon" do
+      assert "user:created:v1:schema:v1" ==
+               Naming.trim_domain_prefix("com:test:user:created:v1:schema:v1")
+    end
+
+    test "only removes first occurence" do
+      assert "user.created.com.test.v1.schema.v1" ==
+               Naming.trim_domain_prefix("com.test.user.created.com.test.v1.schema.v1")
+    end
+  end
+
+  test "version_suffix/1 defaults to version 1" do
+    assert Naming.version_suffix("com:acme:user:created:") ==
+             "com:acme:user:created:v1"
+  end
+
+  test "version_suffix/2 adds version" do
+    assert Naming.version_suffix("com:acme:user:created:", 2) ==
+             "com:acme:user:created:v2"
+  end
+
+  describe "trim_version_suffix/1" do
+    test "with dots" do
+      assert Naming.trim_version_suffix("com.acme.user.created.v1") == "com.acme.user.created"
+    end
+
+    test "with colons" do
+      assert Naming.trim_version_suffix("com:acme:user:created:v1") == "com:acme:user:created"
+    end
+  end
+end
