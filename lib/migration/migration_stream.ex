b04da52cd77cad46a92c54eb4fcfc682825b3966
brandynbennett <brandynlbennett@gmail.com>
2022-06-06 13:13:09 -0600

    can add migration id to migration stream

--- lib/migration/migration_stream.ex
+++ lib/migration/migration_stream.ex
@@ -9,6 +9,7 @@ defmodule Polyn.MigrationStream do
   @migration_subject "POLYN_MIGRATIONS.all"
 
   @doc "Create the migration stream"
+  @spec create() :: {:ok, Stream.info()}
   def create(opts \\ []) do
     stream =
       struct!(Stream, %{
@@ -22,8 +23,25 @@ defmodule Polyn.MigrationStream do
   end
 
   @doc "Get information about the migration stream"
+  @spec info() :: {:ok, Stream.info()}
   def info(opts \\ []) do
     Stream.info(Connection.name(), stream_name(opts))
+  end
+
+  @doc "Add an executed migration to the stream"
+  @spec add_migration(migration_id :: binary()) :: :ok
+  def add_migration(migration_id, opts \\ []) do
+    Gnat.pub(Connection.name(), stream_subject(opts), migration_id)
+  end
+
+  @doc """
+  Get the last run migration from the stream
+  """
+  @spec get_last_migration() :: {:ok, Stream.message_response()}
+  def get_last_migration(opts \\ []) do
+    Stream.get_message(Connection.name(), stream_name(opts), %{
+      last_by_subj: stream_subject(opts)
+    })
   end
 
   defp stream_name(opts) do
--- test/migration/migration_stream_test.exs
+++ test/migration/migration_stream_test.exs
@@ -7,14 +7,26 @@ defmodule Polyn.MigrationStreamTest do
   @subject_name "POLYN_MIGRATIONS_TEST.all"
 
   test "create/1 creates the stream" do
-    assert {:ok, info} = MigrationStream.create(name: @stream_name, subject: @subject_name)
+    assert {:ok, info} = MigrationStream.create(opts())
     assert info.config.name == @stream_name
     assert info.config.subjects == [@subject_name]
   end
 
   test "info/1 gets stream info" do
-    assert {:ok, create_info} = MigrationStream.create(name: @stream_name, subject: @subject_name)
-    assert {:ok, info} = MigrationStream.info(name: @stream_name, subject: @subject_name)
+    assert {:ok, create_info} = MigrationStream.create(opts())
+    assert {:ok, info} = MigrationStream.info(opts())
     assert create_info == info
+  end
+
+  test "add_migration/2 adds a migration id to the stream" do
+    assert {:ok, _info} = MigrationStream.create(opts())
+    assert :ok = MigrationStream.add_migration("123", opts())
+
+    assert {:ok, %{data: data}} = MigrationStream.get_last_migration(opts())
+    assert data == "123"
+  end
+
+  defp opts do
+    [name: @stream_name, subject: @subject_name]
   end
 end
