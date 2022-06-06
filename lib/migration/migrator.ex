7700ecf3fdac49b4513263fef382eb19b6622bf6
brandynbennett <brandynlbennett@gmail.com>
2022-06-06 12:37:06 -0600

    migrator gets files and compiles them

--- lib/migration/migrator.ex
+++ lib/migration/migrator.ex
@@ -2,15 +2,10 @@ defmodule Polyn.Migrator do
   @moduledoc false
   require Logger
   alias Jetstream.API.Stream
-  alias Polyn.Connection
+  alias Polyn.MigrationStream
   alias Polyn.Schema
   alias Polyn.SchemaStore
   alias Polyn.SchemaCompatability
-  alias Polyn.Serializers.JSON
-  alias Polyn.Replicas
-
-  @migration_stream "POLYN_MIGRATIONS"
-  @migration_subject "POLYN_MIGRATIONS.all"
 
   @typedoc """
   * `:migrations_dir` - Location of migration files
@@ -18,6 +13,8 @@ defmodule Polyn.Migrator do
   * `:schema_store_name` - Name of the K/V store where schemas live
   * `:running_migration_id` - The timestamp/id of the migration file being run. Taken from the beginning of the file name
   * `:running_migration_command_num` - The number of the command being run in the migration module
+  * `:migration_files` - The file names of migration files
+  * `:migration_modules` - A list of tuples with the migration id and module code
   * `:already_run_migrations` - Migrations we've determined have already been executed on the server
   * `:production_migrations` - Migrations that have been run on the production server already
   * `:application_migrations` - Migrations that live locally in the codebase
@@ -30,6 +27,8 @@ defmodule Polyn.Migrator do
           running_migration_id: non_neg_integer() | nil,
           running_migration_command_num: non_neg_integer() | nil,
           migration_stream_info: Stream.info() | nil,
+          migration_files: list(binary()),
+          migration_modules: list({integer(), module()}),
           already_run_migrations: list(binary()),
           production_migrations: list(binary()),
           application_migrations: list(binary())
@@ -43,6 +42,8 @@ defmodule Polyn.Migrator do
     :migrations_dir,
     :schemas_dir,
     :schema_store_name,
+    migration_files: [],
+    migration_modules: [],
     already_run_migrations: [],
     production_migrations: [],
     application_migrations: []
@@ -64,6 +65,9 @@ defmodule Polyn.Migrator do
     |> create_migration_stream()
     |> create_schema_store()
     |> add_schemas_to_store()
+    |> get_migration_files()
+    |> compile_migration_files()
+    |> run_migrations()
   end
 
   @doc """
@@ -74,7 +78,7 @@ defmodule Polyn.Migrator do
   end
 
   defp fetch_migration_stream_info(state) do
-    case Stream.info(Connection.name(), @migration_stream) do
+    case MigrationStream.info() do
       {:ok, info} -> Map.put(state, :migration_stream_info, info)
       _ -> state
     end
@@ -83,15 +87,7 @@ defmodule Polyn.Migrator do
   # We'll keep all migrations on a JetStream Stream so that we can
   # keep them in order and know which ones have run already
   defp create_migration_stream(%{migration_stream_info: nil} = state) do
-    stream =
-      struct!(Stream, %{
-        name: @migration_stream,
-        subjects: [@migration_subject],
-        discard: :new,
-        num_replicas: Replicas.three_or_less()
-      })
-
-    case Stream.create(Connection.name(), stream) do
+    case MigrationStream.create() do
       {:error, reason} ->
         raise Polyn.MigrationException, inspect(reason)
 
@@ -118,5 +114,48 @@ defmodule Polyn.Migrator do
     end)
 
     state
+  end
+
+  defp get_migration_files(%{migrations_dir: migrations_dir} = state) do
+    files =
+      case File.ls(migrations_dir) do
+        {:ok, []} ->
+          Logger.info("No migrations found at #{migrations_dir}")
+          []
+
+        {:ok, files} ->
+          files
+          |> Enum.filter(&is_elixir_script?/1)
+          |> Enum.sort_by(&extract_migration_id/1)
+
+        {:error, _reason} ->
+          Logger.info("No migrations found at #{migrations_dir}")
+          []
+      end
+
+    Map.put(state, :migration_files, files)
+  end
+
+  defp is_elixir_script?(file_name) do
+    String.ends_with?(file_name, ".exs")
+  end
+
+  defp extract_migration_id(file_name) do
+    [id | _] = String.split(file_name, "_")
+    String.to_integer(id)
+  end
+
+  defp compile_migration_files(%{migration_files: files, migrations_dir: migrations_dir} = state) do
+    modules =
+      Enum.map(files, fn file_name ->
+        id = extract_migration_id(file_name)
+        [{module, _content}] = Code.compile_file(Path.join(migrations_dir, file_name))
+        {id, module}
+      end)
+
+    Map.put(state, :migration_modules, modules)
+  end
+
+  defp run_migrations(state) do
   end
 end
